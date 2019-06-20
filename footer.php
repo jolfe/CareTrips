c0818b5afa98a385b6aa57050c7641e7426cdd17
jolfe <47413115+jolfe@users.noreply.github.com>
2019-06-20 12:35:43 -0500

    Footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+    <footer class="page-footer font-small special-color-dark pt-4">
+        <div class="container">
+            <ul class="list-unstyled list-inline text-center">
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-fb mx-1">
+                        <a href="index.html">Home</a>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-tw mx-1">
+                        <a href="">Privacy Policy</a>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-gplus mx-1">
+                        <a href="">Sitemap</a>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-gplus mx-1">
+                        <a class="footerLogo" href=""><img src="img/footer.png" alt="">
+                        </a>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </footer>
